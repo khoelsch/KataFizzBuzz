c3e556a02bae254d0f9105749bbb88db654dca75
Kai Hoelscher <Kai.Hoelscher@coremedia.com>
2011-12-08 11:12:58 +0100

    Initial import.

---
+++ src/main/java/com/coremedia/codekatas/FizzBuzz.java
@@ -0,0 +1,12 @@
+package com.coremedia.codekatas;
+
+/**
+ * "Write a program that prints the numbers from 1 to 100. But for multiples
+ * of three print 'Fizz' instead of the number and for the multiples of five
+ * print 'Buzz'. For numbers which are multiples of both three and five print
+ * 'FizzBuz'!"
+ *
+ * For more Details see  <a href="http://codingdojo.org/cgi-bin/wiki.pl?back=KataFizzBuzz">the description @ CodingDojo</a>.
+ */
+public class FizzBuzz {
+}
---
+++ src/test/java/com/coremedia/codekatas/FizzBuzzTest.java
@@ -0,0 +1,13 @@
+package com.coremedia.codekatas;
+
+import org.junit.Test;
+
+public class FizzBuzzTest {
+  @Test
+  public void allowSwiching() {
+   // Exists since only then IDEA recognizes this class as test
+   // class and allows switching between both classes.
+   //
+   // Delete when implementing real tests!
+  }
+}
